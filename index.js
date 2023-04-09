e574c78d84375efec9b8cd326952a1c1cc32b1d6
Sankaram <das95sankaram@gmail.com>
2023-04-09 19:50:19 +0530

    villains

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const superheroes = require('superheroes');
+const supervillains  = require('supervillains');
 
-let mySuperHero = superheroes.random();
+let mySuperVillain = supervillains.random();
 
-console.log(mySuperHero);
+console.log(mySuperVillain );
 
 
-/*
-user.name=DSankaram
-user.email=sankaram@octaloop.com
-*/
