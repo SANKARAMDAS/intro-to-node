61196b9b73b7da9727013767dd119554ccb45265
Sankaram <das95sankaram@gmail.com>
2023-04-11 00:12:17 +0530

    more about req,res

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get('/', function(request, response){
-    response.send('<h1>Hello!</h1>');
+app.get('/', function(req, res){
+    res.send('<h1>Hello!</h1>');
 });
 
-app.listen(3300, function(){
+app.get('/contact', function(req,res){
+    res.send('This is Contact')
+})
+
+app.get('/about', function (req,res){
+    res.send('<h1>This is about myself</h2>')
+})
+
+app.get('/reg', function (req,res){
+    res.send('This is regist')
+})
+app.listen(8080, function(){
     console.log('Server has started port 3300');
 });
