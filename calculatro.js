edb48971e0e43ab15f2743b07cfd6497e317dd80
Sankaram <das95sankaram@gmail.com>
2023-04-11 01:13:24 +0530

    challenge

--- calculatro.js
+++ calculatro.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/bmiCalculator', function (req, res) {
+    res.sendFile(__dirname + '/bmiCalculator.html');
     // console.log(__dirname);
 });
 
-app.post('/', function (req, res){
-    var num1 = Number(req.body.n1);
+app.post('/bmiCalculator', function (req, res){
+    var num1 = parseFloat(req.body.n1);
 
-    var num2 = Number(req.body.n2);
+    var num2 = parseFloat(req.body.n2);
 
-    var result = num1 + num2;
+    var result = n1 / (n2 + n2);
     res.send(result);
 })
 app.listen(8080, function (){
